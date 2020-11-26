23a7d02dd0e3e0ef9665cf5142a12619cafc0c78
Johsua_Kim <joshuara7235@gmail.com>
2020-11-27 00:41:29 +0900

    add baekjoon24264

---
+++ BAEKJOON24264/src/p1/Ex01.java
@@ -0,0 +1,19 @@
+package p1;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		//자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 
+		//구하는 프로그램을 작성하시오
+		//1부터 N까지 자연수중에서 중복없이 M개를 고른 수열
+		//출력 : 한줄에 하나씩 수열을 출력. 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 2개 입력해주세요");
+		
+		
+		
+	}
+}
